18d10372daa228f919367416560d8095243cefe4
Sara716g <166444276+Sara716g@users.noreply.github.com>
2026-06-13 21:00:18 +0300

    Revert "Melat/t 17 ai draft coach (#5)" (#13)
    This reverts commit 97fd317e22dee13551f7efe5a7fa240fee590cc3.

--- backend/src/api/questions/controller/question.controller.js
+++ backend/src/api/questions/controller/question.controller.js
@@ -1,19 +1,3 @@
-import { StatusCodes } from 'http-status-codes';
-import { generateQuestionDraftCoachService } from '../service/geminiTextCoach.service.js';
-
-export const generateQuestionDraftCoachController = async (req, res, next) => {
-  try {
-    const { title, content } = req.body;
-
-    const result = await generateQuestionDraftCoachService({
-      title,
-      content,
-    });
-
-    res.status(StatusCodes.OK).json({
-      success: true,
-      message: 'Draft suggestions generated',
-      data: result,
 import { searchQuestionsSemanticService } from '../services/question.service.js';
 
 export const searchQuestionsSemanticController = async (req, res, next) => {
